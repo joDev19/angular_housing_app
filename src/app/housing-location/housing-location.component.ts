a9678bcc4dee3d4da3335dba489423c6748d334f
Jordy GNANIH <jordygnanihpro@gmail.com>
2023-12-19 12:11:14 +0100

    last-version

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import {HousingLocation} from '../housingLocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
